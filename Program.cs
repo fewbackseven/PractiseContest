804ffb3b40eb282ff86805f4c38de2a4cfc8c0d4
Harshith C <fewbackseven@gmail.com>
2021-09-09 00:37:05 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+namespace Test1
+{
+    class Program
+    {
+        static void Main()
+        {
+            System.Int64 Ta = Int64.Parse(Console.ReadLine());
+            System.Int64[] Ti = new Int64[Ta];
+            int[] count = new int[Ta];
+
+
+            for (int i=0;i<Ta;i++)
+            {
+                Ti[i] = Int64.Parse(Console.ReadLine());
+                string num = Ti[i].ToString();                
+                int ln = num.Length;
+                string[] num1 = new string[ln];
+                for (int j=0;j<ln;j++)
+                {
+                    num1[j] = num.Substring(j, 1);
+                    if (num1[j] == "4")
+                        count[i] = count[i] + 1;
+                }
+
+            }
+
+            for (int i=0;i<Ta;i++)
+            {
+                Console.WriteLine(count[i]);
+            }
+
+
+        }
+    }
+}
---
+++ Test.cs
@@ -0,0 +1,83 @@
+﻿
+using System;
+using System.Numerics;
+using System.Linq;
+
+public class Program
+{
+    public static void Main()
+    {
+        int Reports = int.Parse(Console.ReadLine());
+        string[] result = new string[Reports];
+        
+
+        for(int i=0;i<Reports;i++)
+        {
+            
+            int ln = int.Parse(Console.ReadLine());
+            int count=0;
+            string[] test = new string[ln];
+            string tests = Console.ReadLine();
+
+
+            bool snake=true;
+
+            for(int j=0;j<tests.Length;j++)
+            {
+                test[j] = tests.Substring(j, 1);
+                if (test[j] == "H" || test[j] == "T")
+                    count++;
+            }
+
+            if (count == 0)
+                snake = true;
+            else if (count % 2 != 0)
+                snake = false;   
+            else if(count%2==0)
+            {
+                int? Hindx=null , Tindx=null ;
+                for (int k=0;k<test.Length;k++)
+                {
+                    test[k] = tests.Substring(k, 1);
+                    if (test[k] == "H")
+                    {
+                        if (Hindx != null && k != 0)
+                        {      
+                            snake = false; break;
+                        }
+                        Hindx = k;
+                    }
+
+                    else if (test[k] == "T")
+                    {
+                        if (Tindx != null && k != 0)
+                        {      
+                            snake = false; break;
+                        }
+                        Tindx = k;
+                    }
+
+                    if (Hindx < Tindx)
+                    {
+                        Hindx = null; Tindx = null;
+                        snake = true;
+                    }
+                    else if (Hindx > Tindx && Tindx!=null)
+                    {
+                        snake = false;
+                        break;
+                    }
+
+                }
+                
+            }
+            result[i] = snake == true ? "Valid" : "Invalid";
+
+
+        }
+
+        for (int j = 0; j < Reports; j++)
+            Console.WriteLine(result[j]);
+    }
+  
+}
